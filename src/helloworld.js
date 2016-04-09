50871aebde5ff3c833004a9b6b52410e18d32c96
RamC <ramchn.969@gmail.com>
2016-04-09 21:32:36 +0530

    add source files

---
+++ src/helloworld.js
@@ -0,0 +1,7 @@
+calc = function() {};
+
+console.log("hello world\n");
+
+returnval = calc();
+
+console.log(returnval);
---
+++ src/utils.js
@@ -0,0 +1,22 @@
+function printtext(text) {
+
+	console.log(text);
+
+	return text;
+}
+
+
+function whoami() {
+
+	console.log("I am the warrior");
+
+	return "I am the warrior";
+}
+
+function calc() {
+	var ass = 100;
+	bss = 200;
+	css = ass + bss;
+
+	return css * css;
+}
